23cbcbc51fc853ac0c86fe2ba24b2fdbfdc7af75
VinhChu <VinhCV161@gmail.com>
2020-01-25 00:41:44 +0700

    happy new year 2020!

---
+++ Blogs/js/1.js
@@ -0,0 +1,21 @@
+﻿ $(function(){
+ 	 	 
+ 	 $(".xuong,.kn").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#khoiedu').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+ 	$(".sp").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#khoisanpham').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+ 	
+	$(".cm").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#phanhoi').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+
+})  
+ 
